2394401687c53c4690836258e27a7079bcff0228
alisieck <amadeusz.lisiecki@gmail.com>
2016-12-20 20:45:00 +0100

    ADD command

--- CPU/Microprocessor.cs
+++ CPU/Microprocessor.cs
@@ -17,91 +17,6 @@ namespace CPU
             registers = InitRegisters();
         }
 
-        public void Command(string cmd)
-        {
-            string[] cmdWords = cmd.Split();
-            cmd = cmdWords[0];
-            switch (cmd)
-            {
-                case "MOV":
-                    MOV(cmdWords.Skip(1).ToArray());
-                    break;
-                case "ADD":
-                    //ADD(cmdWords.Skip(1).ToArray());
-                    break;
-                case "SUB":
-                    //SUB(cmdWords.Skip(1).ToArray());
-                    break;
-                default:
-                    throw new NotSupportedException("Not supported command.");
-            }
-        }
-
-        private void MOV(string[] arguments)
-        {
-            if (!IsRegister(arguments[1]))
-                throw new FormatException("Register does not exist.");
-            string destRegister = arguments[1];
-
-            bool[] sourceValue;
-            if (IsRegister(arguments[0]))
-                sourceValue = GetValueFromRegister(arguments[0]);
-            else if (Regex.IsMatch(arguments[0], "^[01]+$") && arguments[0].Length == 8)
-                sourceValue = arguments[0].Select(c => c == '1').ToArray();
-            else
-                throw new FormatException("First argument is not a register nor an 8/16 bits number.");
-
-            WriteToRegister(sourceValue, destRegister);
-        }
-
-        private void WriteToRegister(bool[] value, string register)
-        {
-            if (IsGPR(register))
-            {
-                string rH = register[0] + "H";
-                string rL = register[0] + "L";
-                registers[register][rH] = (bool[]) value.Take(8);
-                registers[register][rL] = (bool[]) value.Skip(8);
-            }
-            else
-            {
-                string gpr = register[0] + "X";
-                registers[gpr][register] = (bool[]) value.Clone();
-            }
-        }
-
-        private bool[] GetValueFromRegister(string register)
-        {
-            if (IsGPR(register))
-            {
-                return (bool[]) registers[register][register[0] + "H"].Concat(registers[register][register[0] + "L"]);
-            }
-            else
-            {
-                return registers[register[0] + "X"][register];
-            }
-        }
-
-        //private void MoveValue(string source, string destination)
-        //{
-        //    string gprSource = source[0] + "X";
-        //    string gprDestination = destination[0] + "X";
-
-        //    registers[gprDestination][destination] = (bool[]) registers[gprSource][source].Clone();
-        //}
-
-        private bool IsRegister(string address)
-        {
-            if (registers.ContainsKey(address)) return true;
-            else if ( registers.ContainsKey(address[0] + "X") && registers[address[0] + "X"].ContainsKey(address)) return true;
-            else return false;
-        }
-
-        private bool IsGPR(string register)
-        {
-            return register[1].Equals("X");
-        }
-
         private SortedDictionary<string, SortedDictionary<string, bool[]>> InitRegisters()
         {
             return new SortedDictionary<string, SortedDictionary<string, bool[]>>
@@ -127,6 +42,167 @@ namespace CPU
                     ["DL"] = Enumerable.Repeat(false, 8).ToArray()
                 }
             };
+        }
+
+        public void Command(string cmd)
+        {
+            string[] cmdWords = cmd.Split();
+            cmd = cmdWords[0];
+            switch (cmd)
+            {
+                case "MOV":
+                    MOV(cmdWords.Skip(1).ToArray());
+                    break;
+                case "ADD":
+                    ADD(cmdWords.Skip(1).ToArray());
+                    break;
+                case "SUB":
+                    //SUB(cmdWords.Skip(1).ToArray());
+                    break;
+                default:
+                    throw new NotSupportedException("Not supported command.");
+            }
+        }
+
+        private void MOV(string[] arguments)
+        {
+            if (!IsRegister(arguments[1]))
+                throw new FormatException("Register does not exist.");
+
+            if (IsRegister(arguments[0]))
+            {
+                if (!IsSameType(arguments[0], arguments[1]))
+                    throw new FormatException("Registers have different sizes.");
+            }
+            else if (!IsSameSize(arguments[0], arguments[1]))
+                throw new FormatException("Value does not match register size.");
+
+            string destRegister = arguments[1];
+            bool[] sourceValue;
+
+            if (IsRegister(arguments[0]))
+                sourceValue = GetValueFromRegister(arguments[0]);
+            else if (Regex.IsMatch(arguments[0], "^[01]+$"))
+                sourceValue = arguments[0].Select(c => c == '1').ToArray();
+            else
+                throw new FormatException("First argument is not a register nor an 8/16 bits number.");
+
+            WriteToRegister(sourceValue, destRegister);
+        }
+
+        private void ADD(string[] arguments)
+        {
+            if (!IsRegister(arguments[1]))
+                throw new FormatException("Register does not exist.");
+
+            if (IsRegister(arguments[0]))
+            {
+                if (!IsSameType(arguments[0], arguments[1]))
+                    throw new FormatException("Registers have different sizes.");
+            }
+            else if (!IsSameSize(arguments[0], arguments[1]))
+                throw new FormatException("Value does not match register size.");
+
+            string destRegister = arguments[1];
+            bool[] sourceValue;
+
+            if (IsRegister(arguments[0]))
+                sourceValue = GetValueFromRegister(arguments[0]);
+            else if (Regex.IsMatch(arguments[0], "^[01]+$"))
+                sourceValue = arguments[0].Select(c => c == '1').ToArray();
+            else
+                throw new FormatException("First argument is not a register nor an 8/16 bits number.");
+
+            AddToRegister(sourceValue, destRegister);
+        }
+
+        private bool IsSameSize(string value, string register)
+        {
+            if (IsGPR(register))
+                return value.Length == 16;
+            else
+                return value.Length == 8;
+        }
+
+        private bool IsSameType(string register1, string register2)
+        {
+            if ((IsGPR(register1) && IsGPR(register2)) || (!IsGPR(register1) && !IsGPR(register2)))
+                return true;
+            else
+                return false;
+        }
+
+        private void WriteToRegister(bool[] value, string register)
+        {
+            if (IsGPR(register))
+            {
+                string rH = register[0] + "H";
+                string rL = register[0] + "L";
+                registers[register][rH] = value.Take(8).Cast<bool>().ToArray();
+                registers[register][rL] = value.Skip(8).Cast<bool>().ToArray();
+            }
+            else
+            {
+                string gpr = register[0] + "X";
+                registers[gpr][register] = (bool[]) value.Clone();
+            }
+        }
+
+        private void AddToRegister(bool[] value, string register)
+        {
+            if (IsGPR(register))
+            {
+                string rH = register[0] + "H";
+                string rL = register[0] + "L";
+                bool[] registerValue = registers[register][rH].Concat(registers[register][rL]).ToArray();
+                registerValue = Adder(value, registerValue);
+                registers[register][rH] = registerValue.Take(8).Cast<bool>().ToArray();
+                registers[register][rL] = registerValue.Skip(8).Cast<bool>().ToArray();
+            }
+            else
+            {
+                string gpr = register[0] + "X";
+                bool[] registerValue = registers[gpr][register];
+                registerValue = Adder(value, registerValue);
+                registers[gpr][register] = (bool[]) registerValue.Clone();
+            }
+        }
+
+        private bool[] Adder(bool[] a, bool[] b)
+        {
+            var sum = new List<bool>(Enumerable.Repeat(false, a.Length));
+            bool carry = false;
+            for (int i = a.Length - 1; i >= 0; i--)
+            {
+                sum[i] = (a[i] ^ b[i]) ^ carry;
+                carry = (a[i] && b[i]) || (carry && (a[i] ^ b[i]));
+            }
+
+            return sum.ToArray();
+        }
+
+        private bool[] GetValueFromRegister(string register)
+        {
+            if (IsGPR(register))
+            {
+                return (bool[]) registers[register][register[0] + "H"].Concat(registers[register][register[0] + "L"]);
+            }
+            else
+            {
+                return registers[register[0] + "X"][register];
+            }
+        }
+
+        private bool IsRegister(string address)
+        {
+            if (registers.ContainsKey(address)) return true;
+            else if ( registers.ContainsKey(address[0] + "X") && registers[address[0] + "X"].ContainsKey(address)) return true;
+            else return false;
+        }
+
+        private bool IsGPR(string register)
+        {
+            return register[1].Equals('X');
         }
 
         public void PrintRegisters()
--- CPU/Program.cs
+++ CPU/Program.cs
@@ -16,12 +16,27 @@ namespace CPU
         {
             Microprocessor mp = new Microprocessor();
             mp.PrintRegisters();
-            mp.Command("MOV 01010101 AH");
+            mp.Command("MOV 0101010111111111 DX");
             Console.WriteLine("----------------------------------");
             mp.PrintRegisters();
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("----------------------------------");
+            mp.Command("MOV DH AL");
+            Console.WriteLine("----------------------------------");
+            mp.PrintRegisters();
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("----------------------------------");
+            mp.Command("ADD 00000001 DL");
+            Console.WriteLine("----------------------------------");
+            mp.PrintRegisters();
+            Console.WriteLine("----------------------------------");
+            mp.Command("ADD 1010101011111100 DX");
+            Console.WriteLine("----------------------------------");
+            mp.PrintRegisters();
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
         }
     }
 }
