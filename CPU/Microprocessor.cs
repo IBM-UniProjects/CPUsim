787d8a13be4445c20c16e61710597aeababcdde6
alisieck <amadeusz.lisiecki@gmail.com>
2017-01-31 14:58:59 +0100

    3 interrupts total

--- CPU/Microprocessor.cs
+++ CPU/Microprocessor.cs
@@ -64,11 +64,128 @@ namespace CPU
                     SUB(cmdWords.Skip(1).ToArray());
                     break;
                 case "INT":
-                    Console.WriteLine(cmd);
+                    INT(cmdWords[0]);
                     break;
                 default:
                     throw new NotSupportedException("Not supported command.");
             }
+        }
+
+        private void INT(string interrupt) {
+            switch (interrupt)
+            {
+                case "INT1":
+                    int n = RegToInt("AH");
+                    Console.WriteLine(n);
+                    bool[] reg = IntToReg(n);
+                    for (int i = 0; i < reg.Length; i++)
+                    {
+                        Console.Write(reg[i] ? 1 : 0);
+                    }
+                    Console.WriteLine();
+                    break;
+                case "INT33":
+                    INT33();
+                    break;
+                default:
+                    throw new NotSupportedException(String.Format("Not supported interrupt {0}.", interrupt));
+            }
+        }
+
+        private void INT33()
+        {
+            int AH = RegToInt("AH");
+            int AL;
+            int CH;
+            int CL;
+            int DH;
+            int DL;
+            switch (AH)
+            {
+                case 1:
+                    /* AH = 01h
+                     * Read character from stdin
+                     * Return: AL = character read
+                     */
+                    FlushStdin();
+                    AL = Console.ReadKey().KeyChar;
+                    Console.WriteLine("\n" + AL);
+                    FlushStdin();
+                    if (AL > 255)
+                        AL = 0;
+                    registers["AX"]["AL"] = IntToReg(AL);
+                    break;
+                case 2:
+                    /* AH = 02h
+                     * Write character to stdout
+                     * Entry: DL = character to write
+                     * Return: AL = last character output
+                     */
+                    DL = RegToInt("DL");
+                    Console.WriteLine(Convert.ToChar(DL));
+                    if (DL == 9)
+                        AL = 32;
+                    else
+                        AL = DL;
+                    registers["AX"]["AL"] = IntToReg(AL);
+                    break;
+                case 44:
+                    /* AH = 2Ch
+                     * Get system time
+                     * Return: CH: hour, CL: minute, DH: second, DL: 1/100 second
+                     */
+                    DateTime time = DateTime.Now;
+                    CH = time.Hour;
+                    CL = time.Minute;
+                    DH = time.Second;
+                    DL = time.Millisecond / 10;
+                    Console.WriteLine(time.ToString("HH:mm:ss:ff"));
+                    registers["CX"]["CH"] = IntToReg(CH);
+                    registers["CX"]["CL"] = IntToReg(CL);
+                    registers["DX"]["DH"] = IntToReg(DH);
+                    registers["DX"]["DL"] = IntToReg(DL);
+                    break;
+                default:
+                    throw new NotSupportedException(String.Format("Not supported interrupt INT33, AH = {0}.", AH));
+            }
+        }
+
+        private void FlushStdin()
+        {
+            while (Console.KeyAvailable)
+                Console.ReadKey(true);
+        }
+
+        private int RegToInt(string register)
+        {
+            bool[] reg;
+            int n = 0;
+
+            if (IsGPR(register))
+                throw new FormatException("16-bit register not supported.");
+
+            string gpr = register[0] + "X";
+            reg = (bool[]) registers[gpr][register].Clone();
+
+            for (int i = 0; i < reg.Length; i++)
+            {
+                n = (n << 1) + (reg[i] ? 1 : 0);
+            }
+
+            return n;
+        }
+
+        private bool[] IntToReg(int n)
+        {
+            if (n > 255)
+                throw new FormatException("Max 8-bit number.");
+
+            bool[] reg = new bool[8];
+            for (int i = 0; i < 8; i++)
+            {
+                reg[7 - i] = (1 << i & n) != 0;
+            }
+            return reg;
         }
 
         private void MOV(string[] arguments)
